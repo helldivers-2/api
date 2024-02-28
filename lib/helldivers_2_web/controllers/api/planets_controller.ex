a36e678e82d1fac645299ec54b8bc34b0836f208
Wannes Gennar <wannes.gennar@gmail.com>
2024-02-28 10:02:46 +0100

    hotfix: use route params instead of assigns
    War ID is passed through route params instead of assigns on the
    Plug.Conn

--- lib/helldivers_2_web/controllers/api/planets_controller.ex
+++ lib/helldivers_2_web/controllers/api/planets_controller.ex
@@ -3,7 +3,7 @@ defmodule Helldivers2Web.Api.PlanetsController do
 
   alias Helldivers2.WarSeason
 
-  def index(%Plug.Conn{assigns: %{war_id: war_id}} = conn, _) do
+  def index(conn, %{"war_id" => war_id}) do
     with {:ok, planets} <- WarSeason.get_planets(war_id) do
       render(conn, :index, planets: planets)
     end
