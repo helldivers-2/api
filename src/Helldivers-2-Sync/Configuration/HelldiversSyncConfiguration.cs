39983ed303798513bde8d0d5b8fb388a2c3b12eb
Wannes Gennar <wannes.gennar@gmail.com>
2024-03-28 15:01:47 +0100

    setup localization
    see #1
    see #14

--- src/Helldivers-2-Sync/Configuration/HelldiversSyncConfiguration.cs
+++ src/Helldivers-2-Sync/Configuration/HelldiversSyncConfiguration.cs
@@ -5,7 +5,13 @@
 /// </summary>
 public sealed class HelldiversSyncConfiguration
 {
+    /// <summary>
+    /// The interval (in seconds) at which the sync services will fetch information from the API.
+    /// </summary>
     public int IntervalSeconds { get; set; } = 10;
 
+    /// <summary>
+    /// A list of language codes for which translations will be provided.
+    /// </summary>
     public List<string> Languages { get; set; } = new(0);
 }
