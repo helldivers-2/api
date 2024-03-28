b658cde66cd0c43c61a7250ff1295af20606dfd0
Wannes Gennar <wannes.gennar@gmail.com>
2024-03-28 15:01:46 +0100

    scaffold .NET API

--- lib/helldivers_2_web/controllers/page_controller.ex
+++
@@ -1,9 +0,0 @@
-defmodule Helldivers2Web.PageController do
-  use Helldivers2Web, :controller
-
-  def home(conn, _params) do
-    # The home page is often custom made,
-    # so skip the default app layout.
-    render(conn, :home, layout: false)
-  end
-end
