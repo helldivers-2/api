6a1d76c8797e542c13faf02ce9a6a45c79d41456
Wannes Gennar <wannes.gennar@gmail.com>
2024-03-28 15:01:47 +0100

    setup configuration and sync service

---
+++ src/Helldivers-2-Sync/Configuration/HelldiversSyncConfiguration.cs
@@ -0,0 +1,9 @@
+﻿namespace Helldivers.Sync.Configuration;
+
+/// <summary>
+/// Represents configuration for the synchronization system of the Helldivers 2 API.
+/// </summary>
+public sealed class HelldiversSyncConfiguration
+{
+    public int IntervalSeconds { get; set; } = 10;
+}
