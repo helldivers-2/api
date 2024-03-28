64e8ebb659c743e740d2fec69a62460e833d5b32
Wannes Gennar <wannes.gennar@gmail.com>
2024-03-28 15:01:46 +0100

    strip OpenAPI dependencies from release builds

--- src/Helldivers-2-API/Program.cs
+++ src/Helldivers-2-API/Program.cs
@@ -3,9 +3,12 @@ using Helldivers.API.Controllers;
 var builder = WebApplication.CreateSlimBuilder(args);
 
 builder.Services.AddProblemDetails();
-builder.Services.AddEndpointsApiExplorer();
 
+#if DEBUG
+// Swagger is generated at compile time, so we don't include Swagger dependencies in Release builds.
+builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+#endif
 
 builder.Services.ConfigureHttpJsonOptions(options =>
 {
