c92a6b328860ab20d19729be11245873c58ce337
Wannes Gennar <wannes.gennar@gmail.com>
2024-03-28 15:01:47 +0100

    add mapping of ArrowHead's official API

---
+++ src/Helldivers-2-Models/ArrowHead/Assignment.cs
@@ -0,0 +1,17 @@
+﻿using Helldivers.Models.ArrowHead.Assignments;
+
+namespace Helldivers.Models.ArrowHead;
+
+/// <summary>
+/// Represents an assignment given from Super Earth to the Helldivers.
+/// </summary>
+/// <param name="Id32">Internal identifier of this assignment.</param>
+/// <param name="Progress">A list of numbers, how they represent progress is unknown.</param>
+/// <param name="ExpiresIn">The amount of seconds until this assignment expires.</param>
+/// <param name="Setting">Contains detailed information on this assignment like briefing, rewards, ...</param>
+public sealed record Assignment(
+    int Id32,
+    List<int> Progress,
+    long ExpiresIn,
+    Setting Setting
+);
---
+++ src/Helldivers-2-Models/ArrowHead/Status/Campaign.cs
@@ -0,0 +1,15 @@
+﻿namespace Helldivers.Models.ArrowHead.Status;
+
+/// <summary>
+/// Contains information of ongoing campaigns.
+/// </summary>
+/// <param name="Id">The identifier of this campaign.</param>
+/// <param name="PlanetIndex">The <see cref="PlanetStatus.Index" /> of the planet this campaign refers to.</param>
+/// <param name="Type">A numerical type, purpose unknown.</param>
+/// <param name="Count">A numerical count, unknown what it counts.</param>
+public sealed record Campaign(
+    int Id,
+    int PlanetIndex,
+    int Type,
+    int Count
+);
