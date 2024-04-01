4014eb5aca8b261e9af871f4edbee0d55819f38b
Wannes Gennar <wannes.gennar@gmail.com>
2024-04-01 21:04:13 +0200

    add test projects

---
+++ tests/Helldivers-2-Api-Test/UnitTest1.cs
@@ -0,0 +1,9 @@
+namespace Helldivers_2_Api_Test;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+    }
+}
---
+++ tests/Helldivers-2-Core-Test/UnitTest1.cs
@@ -0,0 +1,9 @@
+namespace Helldivers.Core.Test;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+    }
+}
