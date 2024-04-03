dae435b2e9518724b92093b74537d1fb45ceabdb
TheDarkGlobe <lambstream@gmail.com>
2024-04-03 23:59:29 +0200

    docs: XP => influence

--- src/Helldivers-2-Models/ArrowHead/WarStatus.cs
+++ src/Helldivers-2-Models/ArrowHead/WarStatus.cs
@@ -7,7 +7,7 @@ namespace Helldivers.Models.ArrowHead;
 /// </summary>
 /// <param name="WarId">The war season this snapshot refers to.</param>
 /// <param name="Time">The time this snapshot was taken.</param>
-/// <param name="ImpactMultiplier">This is the factor by which XP at the end of a mission is multiplied to calculate the impact on liberation</param>
+/// <param name="ImpactMultiplier">This is the factor by which influence at the end of a mission is multiplied to calculate the impact on liberation</param>
 /// <param name="StoryBeatId32">Internal identifier, purpose unknown.</param>
 /// <param name="PlanetStatus">A list of statuses for planets.</param>
 /// <param name="PlanetAttacks">A list of attacks currently ongoing.</param>
