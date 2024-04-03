0b36e727fa0d35a05df5d45afb23a08870b322c5
TheDarkGlobe <lambstream@gmail.com>
2024-04-04 00:33:11 +0200

    Docs/fix spelling (#37)
    * docs: therefor -> therefore
    
    * docs: XP => influence

--- src/Helldivers-2-Models/ArrowHead/Assignments/Task.cs
+++ src/Helldivers-2-Models/ArrowHead/Assignments/Task.cs
@@ -2,7 +2,7 @@
 
 /// <summary>
 /// Represents a task in an <see cref="Assignment" />.
-/// It's exact values are not known, therefor little of it's purpose is clear.
+/// It's exact values are not known, therefore little of it's purpose is clear.
 /// </summary>
 /// <param name="Type">A numerical value, purpose unknown.</param>
 /// <param name="Values">A list of numerical values, purpose unknown.</param>
