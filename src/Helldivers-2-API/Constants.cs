36f2a1bb5b91f7f8c87c0671b71cbd6df8ad4d59
Wannes Gennar <wannes.gennar@gmail.com>
2024-04-30 22:52:49 +0200

    allow X-Super headers in CORS
    see ashortsleeves/heckdivers#2

---
+++ src/Helldivers-2-API/Constants.cs
@@ -0,0 +1,17 @@
+﻿namespace Helldivers.API;
+
+/// <summary>
+/// Contains constant strings used througout the API.
+/// </summary>
+public static class Constants
+{
+    /// <summary>
+    /// The name of the header that identifies the client to the API.
+    /// </summary>
+    public static string CLIENT_HEADER_NAME = "X-Super-Client";
+
+    /// <summary>
+    /// The name of the header with developer contact information.
+    /// </summary>
+    public static string CONTACT_HEADER_NAME = "X-Super-Contact";
+}
