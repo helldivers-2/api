a04b920940359193251e907a3b36bab2d9c41625
Wannes Gennar <wannes.gennar@gmail.com>
2024-11-15 00:16:36 +0100

    add v1 support for space stations

--- src/Helldivers-2-Models/ArrowHead/Status/SpaceStation.cs
+++ src/Helldivers-2-Models/ArrowHead/Status/SpaceStation.cs
@@ -3,10 +3,10 @@ namespace Helldivers.Models.ArrowHead.Status;
 /// <summary>
 /// Represents one of the space stations as passed from the ArrowHead API.
 /// </summary>
-/// <param name="Id32"></param>
-/// <param name="PlanetIndex"></param>
-/// <param name="CurrentElectionEndWarTime"></param>
-/// <param name="Flags"></param>
+/// <param name="Id32">The unique identifier of the station.</param>
+/// <param name="PlanetIndex">The id of the planet it's currently orbiting</param>
+/// <param name="CurrentElectionEndWarTime">When the election for the next planet will end (in seconds relative to game start).</param>
+/// <param name="Flags">A set of flags, purpose currently unknown.</param>
 public sealed record SpaceStation(
     long Id32,
     int PlanetIndex,
---
+++ src/Helldivers-2-Models/V1/SpaceStation.cs
@@ -0,0 +1,15 @@
+namespace Helldivers.Models.V1;
+
+/// <summary>
+/// Represents a Super Earth Democracy Space Station.
+/// </summary>
+/// <param name="Id32">The unique identifier of the station.</param>
+/// <param name="Planet">The planet it's currently orbiting.</param>
+/// <param name="ElectionEnd">When the election for the next planet will end.</param>
+/// <param name="Flags">A set of flags, purpose currently unknown.</param>
+public sealed record SpaceStation(
+    long Id32,
+    Planet Planet,
+    DateTime ElectionEnd,
+    int Flags
+);
