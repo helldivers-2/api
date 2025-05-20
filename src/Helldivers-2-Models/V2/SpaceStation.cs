0bff7a254f23a42e2601923c398462b1282d5431
Wannes Gennar <wannes.gennar@gmail.com>
2025-05-20 14:28:15 +0200

    fix CI warnings

--- src/Helldivers-2-Models/V2/SpaceStation.cs
+++ src/Helldivers-2-Models/V2/SpaceStation.cs
@@ -10,6 +10,7 @@ namespace Helldivers.Models.V2;
 /// <param name="Planet">The planet it's currently orbiting.</param>
 /// <param name="ElectionEnd">When the election for the next planet will end.</param>
 /// <param name="Flags">A set of flags, purpose currently unknown.</param>
+/// <param name="TacticalActions">A list of tactical actions the space station supports.</param>
 public sealed record SpaceStation(
     long Id32,
     Planet Planet,
--- src/Helldivers-2-Models/V2/SpaceStations/Cost.cs
+++ src/Helldivers-2-Models/V2/SpaceStations/Cost.cs
@@ -1,5 +1,15 @@
 ﻿namespace Helldivers.Models.V2.SpaceStations;
 
+/// <summary>
+/// Represents the "Cost" of a tactical action
+/// </summary>
+/// <param name="Id"></param>
+/// <param name="ItemMixId"></param>
+/// <param name="TargetValue"></param>
+/// <param name="CurrentValue"></param>
+/// <param name="DeltaPerSecond"></param>
+/// <param name="MaxDonationAmmount"></param>
+/// <param name="MaxDonationPeriodSeconds"></param>
 public sealed record Cost(
     string Id,
     long ItemMixId,
