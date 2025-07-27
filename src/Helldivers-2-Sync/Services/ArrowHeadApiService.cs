468ced850abed50f0e6fa2a9b74860aa655e14ee
CrosswaveOmega <62901101+CrosswaveOmega@users.noreply.github.com>
2025-07-27 17:52:31 -0400

    Update ArrowHeadApiService.cs
    Co-authored-by: Wannes Gennar <dealloc@users.noreply.github.com>

--- src/Helldivers-2-Sync/Services/ArrowHeadApiService.cs
+++ src/Helldivers-2-Sync/Services/ArrowHeadApiService.cs
@@ -74,7 +74,7 @@ public sealed class ArrowHeadApiService(
     public async Task<Memory<byte>> LoadFeed(string season, string language, CancellationToken cancellationToken)
     {
         // If the `NewsFeedMaxEntries` flag is not set to 0 we pass it in.
-        // fromTimestamp is in options.Value.  This parameter is needed or else a 400 error will be reached every so often.
+        // This parameter needs to be passed or a 400 status code will be returned occasionally.
         var request = options.Value.NewsFeedMaxEntries is 0
             ? BuildRequest($"/api/NewsFeed/{season}?fromTimestamp={options.Value.NewsFeedFromTimestamp}", language)
             : BuildRequest($"/api/NewsFeed/{season}?maxEntries={options.Value.NewsFeedMaxEntries}&fromTimestamp={options.Value.NewsFeedFromTimestamp}", language);
